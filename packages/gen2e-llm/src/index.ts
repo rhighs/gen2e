e99e1665e65679238b436ddaa2be73190656917c
Roberto Montalti <roberto.montalti00@gmail.com>
2024-05-30 11:01:11 +0200

    gen2e-llm: better model support checks and typing

--- packages/gen2e-llm/src/index.ts
+++ packages/gen2e-llm/src/index.ts
@@ -1,3 +1,5 @@
+import { Gen2ELLMAgentOpenAIModel, Gen2ELLMAgentOpenAIModels } from "./types";
+
 export * from "./code-gen-agent";
 export * from "./tools";
 export * from "./sanity";
@@ -5,3 +7,7 @@ export * from "./runner";
 export * from "./errors";
 export * from "./types";
 export * from "./runner/openai-token";
+
+export const isModelSupported = (
+  model: string
+): model is Gen2ELLMAgentOpenAIModel => model in Gen2ELLMAgentOpenAIModels;
--- packages/gen2e-llm/src/types.ts
+++ packages/gen2e-llm/src/types.ts
@@ -89,20 +89,27 @@ export interface Gen2ELLMAgentRunner {
   }>;
 }
 
-export type Gen2ELLMAgentOpenAIModel =
-  | "gpt-3.5-turbo"
-  | "gpt-3.5-turbo-0125"
-  | "gpt-3.5-turbo-0301"
-  | "gpt-3.5-turbo-0613"
-  | "gpt-3.5-turbo-1106"
-  | "gpt-3.5-turbo-16k"
-  | "gpt-3.5-turbo-16k-0613"
-  | "gpt-4"
-  | "gpt-4-1106-preview"
-  | "gpt-4-turbo-preview"
-  | "gpt-4-turbo"
-  | "gpt-4-turbo-2024-04-09"
-  | "gpt-4o"
-  | "gpt-4o-2024-05-13";
+export const Gen2ELLMAgentOpenAIModels = {
+  "gpt-3.5-turbo": true,
+  "gpt-3.5-turbo-0125": true,
+  "gpt-3.5-turbo-0301": true,
+  "gpt-3.5-turbo-0613": true,
+  "gpt-3.5-turbo-1106": true,
+  "gpt-3.5-turbo-16k": true,
+  "gpt-3.5-turbo-16k-0613": true,
+  "gpt-4": true,
+  "gpt-4-1106-preview": true,
+  "gpt-4-turbo-preview": true,
+  "gpt-4-turbo": true,
+  "gpt-4-turbo-2024-04-09": true,
+  "gpt-4o": true,
+  "gpt-4o-2024-05-13": true,
+} as const;
 
-export type Gen2ELLMAgentModel = Gen2ELLMAgentOpenAIModel;
+export const Gen2ELLMAgentModels = {
+  ...Gen2ELLMAgentOpenAIModels,
+} as const;
+
+export type Gen2ELLMAgentOpenAIModel = keyof typeof Gen2ELLMAgentOpenAIModels;
+
+export type Gen2ELLMAgentModel = keyof typeof Gen2ELLMAgentModels;
