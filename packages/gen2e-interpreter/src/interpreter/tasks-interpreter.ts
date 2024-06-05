74fccfd14df0b169f32142a5c121e8c2d70c944a
Roberto Montalti <roberto.montalti00@gmail.com>
2024-06-05 18:27:35 +0200

    gen2e-interpreter: default timeout stat

--- packages/gen2e-interpreter/src/interpreter/tasks-interpreter.ts
+++ packages/gen2e-interpreter/src/interpreter/tasks-interpreter.ts
@@ -25,8 +25,15 @@ test(
     ${
       includeTimeout
         ? `
-    const GEN2E_CALLS_TIMEOUT = 300000;
-    test.setTimeout(GEN2E_CALLS_TIMEOUT);
+    // safety timeout block:
+    //  this is generated and is put here to ensure all gen calls complete correctly in a somewhat
+    //  expected amount of time. if this test block fails due to time constraints feel free
+    //  to increase \`GEN2E_CALLS_TIMEOUT\`.
+    {
+      page.setDefaultTimeout(5000);
+      const GEN2E_CALLS_TIMEOUT = 300000;
+      test.setTimeout(GEN2E_CALLS_TIMEOUT);
+    }
     `
         : ""
     }
