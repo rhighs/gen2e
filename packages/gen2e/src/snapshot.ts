6b5c8010030eaca29568e40435b5eca5c79ab88d
Roberto Montalti <roberto.montalti00@gmail.com>
2024-06-06 12:18:06 +0200

    gen2e: screenshotting debug enhancements

--- packages/gen2e/src/snapshot.ts
+++ packages/gen2e/src/snapshot.ts
@@ -2,111 +2,122 @@ import sanitize, { AllowedAttribute } from "sanitize-html";
 
 import { Page } from "./types";
 import { Gen2ELogger } from "@rhighs/gen2e-logger";
+import os from "node:os";
+import path from "node:path";
+import fs from "node:fs";
+
+const tags: { [key: string]: false | string[] } = {
+  none: false,
+  medium: sanitize.defaults.allowedTags.concat([
+    "html",
+    "body",
+    "option",
+    "button",
+    "form",
+    "frameset",
+    "img",
+    "input",
+    "iframe",
+    "frame",
+    "select",
+    "textarea",
+    "table",
+    "caption",
+    "font",
+    "b",
+    "tbody",
+    "tr",
+    "td",
+    "font",
+    "a",
+    "b",
+    "td",
+    "font",
+    "tr",
+    "td",
+    "font",
+    "a",
+    "b",
+    "td",
+    "font",
+    "tr",
+    "td",
+    "font",
+    "a",
+    "b",
+    "td",
+    "font",
+    "tr",
+  ]),
+  high: [
+    "html",
+    "header",
+    "frameset",
+    "iframe",
+    "section",
+    "body",
+    "div",
+    "input",
+    "button",
+    "form",
+  ],
+};
+
+const attributes: {
+  [key: string]: false | Record<string, AllowedAttribute[]>;
+} = {
+  none: false,
+  medium: {
+    "*": [
+      "alt",
+      "contenteditable",
+      "form",
+      "id",
+      "label",
+      "src",
+      "value",
+      "name",
+      "placeholder",
+      "type",
+      "href",
+      "role",
+      "title",
+      "aria-label",
+      "aria-labelledby",
+      "data-testid",
+      "data-*",
+      "for",
+      "textContent",
+    ],
+    iframe: ["src"],
+    frame: ["src"],
+  },
+  high: {
+    "*": ["id", "placeholder", "data-testid", "textContent"],
+    iframe: ["src"],
+    frame: ["src"],
+  },
+};
 
 const sanitizeHtml = (
   subject: string,
-  stripLevel: "high" | "medium" | "none" = "medium",
+  allowedTags: false | string[] | undefined,
+  allowedAttributes:
+    | false
+    | Record<string, sanitize.AllowedAttribute[]>
+    | undefined,
   logger?: Gen2ELogger
 ) => {
-  const tags: { [key: string]: false | string[] | undefined } = {
-    none: false,
-    medium: sanitize.defaults.allowedTags.concat([
-      "html",
-      "body",
-      "option",
-      "button",
-      "form",
-      "frameset",
-      "img",
-      "input",
-      "iframe",
-      "frame",
-      "select",
-      "textarea",
-      "table",
-      "caption",
-      "font",
-      "b",
-      "tbody",
-      "tr",
-      "td",
-      "font",
-      "a",
-      "b",
-      "td",
-      "font",
-      "tr",
-      "td",
-      "font",
-      "a",
-      "b",
-      "td",
-      "font",
-      "tr",
-      "td",
-      "font",
-      "a",
-      "b",
-      "td",
-      "font",
-      "tr",
-    ]),
-    high: [
-      "html",
-      "header",
-      "frameset",
-      "iframe",
-      "section",
-      "body",
-      "div",
-      "input",
-      "button",
-      "form",
-    ],
-  };
-  const attributes: {
-    [key: string]: false | Record<string, AllowedAttribute[]> | undefined;
-  } = {
-    none: false,
-    medium: {
-      "*": [
-        "alt",
-        "contenteditable",
-        "form",
-        "id",
-        "label",
-        "src",
-        "value",
-        "name",
-        "placeholder",
-        "type",
-        "href",
-        "role",
-        "title",
-        "aria-label",
-        "aria-labelledby",
-        "data-testid",
-        "data-*",
-        "for",
-        "textContent",
-      ],
-      iframe: ["src"],
-      frame: ["src"],
-    },
-    high: {
-      "*": ["id", "placeholder", "data-testid", "textContent"],
-      iframe: ["src"],
-      frame: ["src"],
-    },
-  };
-
   if (logger) {
-    logger.debug("sanitizing html, preserving data ", { tags, attributes });
+    logger.debug("sanitizing html, preserving data ", {
+      allowedTags,
+      allowedAttributes,
+    });
   }
 
   const s = sanitize(subject, {
-    allowedTags: tags[stripLevel],
-    allowedAttributes: attributes[stripLevel],
+    allowedTags,
+    allowedAttributes,
   });
 
   if (logger) {
@@ -124,6 +135,9 @@ export type WebSnapshotOptions = {
   stripLevel?: "high" | "medium" | "none";
   screenshot?: boolean;
   screenshotFullPage?: boolean;
+  saveScreenShot?: boolean;
+  pageOutlines?: boolean;
+  pageDataTags?: boolean;
   debug?: boolean;
 };
 
@@ -157,11 +171,15 @@ export const getSnapshot = async (
     }
   }
 
+  const _tags = tags[opts?.stripLevel ?? "medium"];
+  const _attrs = attributes[opts?.stripLevel ?? "medium"];
+
   const pageContent = mainPageContent + framesContent.join("\n");
   const strippedPageContent = pageContent.replace(/[\t\r\n]/g, "");
   const content = sanitizeHtml(
     strippedPageContent,
-    opts?.stripLevel,
+    _tags,
+    _attrs,
     logger
   ).replace(/[\t\r\n]/g, "");
   if (logger) {
@@ -173,11 +191,95 @@ export const getSnapshot = async (
   };
 
   if (opts?.screenshot) {
+    // rob:
+    // outline every element in the page, this will help better recognising
+    // which element fall into a specific html tree branch.
+    // html tags are also added if specifically asked to via opts, this will show
+    // a floating text at the left-top corner of each element saying what kind of html tag
+    // the outlined element is.
+    if (opts.pageOutlines) {
+      const outline = {
+        content: `
+      * {
+        border: 1px solid black !important;
+      }
+      `,
+      };
+      if (opts.pageDataTags) {
+        const metaTagName = {
+          content: `
+          *::before {
+              content: attr(data-tag);
+              display: block;
+              position: absolute;
+              top: 0;
+              left: 0;
+              color: red;
+              font-size: 12px;
+              border: 1px solid black;
+              padding: 2px;
+              z-index: 9999;
+          }
+
+          * {
+            position: relative;
+          }
+
+          body * {
+              counter-reset: el-counter;
+          }
+
+          body *::before {
+              counter-increment: el-counter;
+              content: attr(data-tag) " " counter(el-counter);
+          }
+      `,
+        };
+
+        for (let p of [...frames, page]) {
+          await p.addStyleTag(metaTagName);
+          await p.evaluate(
+            (_tags) =>
+              document.querySelectorAll("*").forEach((el) => {
+                if (Array.isArray(_tags)) {
+                  if (_tags.includes(el.tagName)) {
+                    el.setAttribute("data-tag", el.tagName.toLowerCase());
+                  }
+                }
+              }),
+            _tags
+          );
+        }
+      }
+
+      for (let p of [...frames, page]) {
+        await p.addStyleTag(outline);
+      }
+    }
+
+    // rob:
+    // this is fixed due to jpeg being the only way to control output image size via `quality` param.
     const buffer = await page.screenshot({
       type: "jpeg",
       fullPage: opts.screenshotFullPage ?? false,
       quality: 20,
     });
+
+    if (opts.saveScreenShot) {
+      const url = new URL(page.url());
+      const filename = `${+new Date()}.jpeg`;
+      const dir = path.join(os.tmpdir(), `gen2e-snapshots`, url.hostname);
+      fs.mkdirSync(dir, {
+        recursive: true,
+      });
+      const filepath = path.join(dir, filename);
+      fs.writeFileSync(filepath, buffer, {
+        flag: "wx",
+      });
+      if (logger) {
+        logger.info(`dom screenshot captured`, { filepath });
+      }
+    }
 
     if ((!buffer || buffer.length === 0) && opts.debug) {
       logger?.debug(
