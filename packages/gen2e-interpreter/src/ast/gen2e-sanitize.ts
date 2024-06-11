7c1573d55ce681e4eb791e1b3b1a32b79897ca05
Roberto Montalti <roberto.montalti00@gmail.com>
2024-06-11 11:50:59 +0200

    chore: minor code tidy up

--- packages/gen2e-interpreter/src/ast/gen2e-sanitize.ts
+++ packages/gen2e-interpreter/src/ast/gen2e-sanitize.ts
@@ -8,147 +8,130 @@ import {
 import { makeCompiler } from "./compiler";
 import { Gen2ELogger } from "@rhighs/gen2e-logger";
 
+const isGenCall = (e: AwaitExpression | CallExpression): boolean => {
+  if (e.type === "AwaitExpression" && e.argument?.type === "CallExpression") {
+    const { callee } = e.argument;
+    const callNameMatches =
+      callee.type === "Identifier" && callee.name === "gen";
+    const hasCorrectArgs =
+      e.argument.arguments.length === 2 &&
+      (e.argument.arguments[0].type === "Literal" ||
+        e.argument.arguments[0].type === "StringLiteral" ||
+        e.argument.arguments[0].type === "TemplateLiteral") &&
+      e.argument.arguments[1].type === "ObjectExpression";
+    return callNameMatches && hasCorrectArgs;
+  } else if (e.type === "CallExpression") {
+    const { callee } = e;
+    const callNameMatches =
+      callee.type === "Identifier" && callee.name === "gen";
+    const hasCorrectArgs =
+      e.arguments.length === 2 &&
+      (e.arguments[0].type === "Literal" ||
+        e.arguments[0].type === "StringLiteral" ||
+        e.arguments[0].type === "TemplateLiteral") &&
+      e.arguments[1].type === "ObjectExpression";
+    return callNameMatches && hasCorrectArgs;
+  }
+  return false;
+};
+
+const resolveChainedMemberCall = (
+  e: MemberExpression
+): CallExpression | undefined => {
+  if (
+    e.object.type === "CallExpression" &&
+    e.object.callee.type === "MemberExpression"
+  ) {
+    return resolveChainedMemberCall(e.object.callee);
+  }
+  if (e.object.type === "CallExpression") {
+    return e.object;
+  }
+  if (
+    e.object.type === "AwaitExpression" &&
+    e.object.argument?.type === "CallExpression"
+  ) {
+    return e.object.argument;
+  }
+  return undefined;
+};
+
+const filterNode = (node: any): boolean => {
+  const maybeChainedV =
+    node.type === "ExpressionStatement" &&
+    node.expression.type === "CallExpression" &&
+    node.expression.callee.type === "MemberExpression"
+      ? resolveChainedMemberCall(node.expression.callee)
+      : undefined;
+
+  if (
+    (node.type === "ExpressionStatement" &&
+      node.expression.type === "CallExpression" &&
+      node.expression.arguments?.length > 0 &&
+      node.expression.arguments[0].type === "AwaitExpression" &&
+      isGenCall(node.expression.arguments[0])) ||
+    (node.type === "ExpressionStatement" &&
+      node.expression.type === "AwaitExpression" &&
+      node.expression.argument?.type === "CallExpression" &&
+      node.expression.argument.arguments[0]?.type === "AwaitExpression" &&
+      isGenCall(node.expression.argument.arguments[0])) ||
+    (maybeChainedV &&
+      maybeChainedV.arguments.length > 0 &&
+      maybeChainedV.arguments[0].type === "AwaitExpression" &&
+      maybeChainedV.arguments[0].argument?.type === "CallExpression" &&
+      isGenCall(maybeChainedV.arguments[0]))
+  ) {
+    throw new Error(
+      "Gen2ESanitize gen2e code error, cannot sanitize gen2e calls as argument to other call expressions"
+    );
+  }
+
+  if (
+    node.type === "VariableDeclaration" &&
+    node.declarations[0].type === "VariableDeclarator" &&
+    node.declarations[0].init?.type === "AwaitExpression" &&
+    isGenCall(node.declarations[0].init)
+  ) {
+    return true;
+  }
+
+  if (
+    node.type === "ExpressionStatement" &&
+    node.expression.type === "AwaitExpression" &&
+    isGenCall(node.expression)
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+const sanitizeGenCalls = (root: any, j: any): void => {
+  root
+    .find(j.CallExpression, {
+      callee: {
+        type: "MemberExpression",
+        object: { name: "gen" },
+        property: { name: "test" },
+      },
+    })
+    .forEach((path: any) => {
+      const arrowFunction = path.node.arguments[0];
+      if (
+        arrowFunction &&
+        arrowFunction.type === "ArrowFunctionExpression" &&
+        arrowFunction.body.type === "BlockStatement"
+      ) {
+        const body = arrowFunction.body.body;
+        arrowFunction.body.body = body.filter(filterNode);
+      }
+    });
+};
+
 export const gen2eSanitize = (source: string) =>
   makeCompiler((fileInfo: FileInfo, api: API, logger?: Gen2ELogger) => {
     const { j } = api;
     const root = j(fileInfo.source);
-
-    const isGenCall = (e: AwaitExpression | CallExpression): boolean => {
-      if (e.type === "AwaitExpression") {
-        if (e.argument?.type === "CallExpression") {
-          const { callee } = e.argument;
-          const callNameMatches =
-            callee.type === "Identifier" && callee.name === "gen";
-          const hasCorrectArgs =
-            e.argument.arguments.length === 2 &&
-            (e.argument.arguments[0].type === "Literal" ||
-              e.argument.arguments[0].type === "StringLiteral" ||
-              e.argument.arguments[0].type === "TemplateLiteral") &&
-            e.argument.arguments[1].type === "ObjectExpression";
-          return callNameMatches && hasCorrectArgs;
-        }
-      } else if (e.type === "CallExpression") {
-        const { callee } = e;
-        const callNameMatches =
-          callee.type === "Identifier" && callee.name === "gen";
-        const hasCorrectArgs =
-          e.arguments.length === 2 &&
-          (e.arguments[0].type === "Literal" ||
-            e.arguments[0].type === "StringLiteral" ||
-            e.arguments[0].type === "TemplateLiteral") &&
-          e.arguments[1].type === "ObjectExpression";
-        return callNameMatches && hasCorrectArgs;
-      }
-
-      return false;
-    };
-
-    root
-      .find(j.CallExpression, {
-        callee: {
-          type: "MemberExpression",
-          object: { name: "gen" },
-          property: { name: "test" },
-        },
-      })
-      .forEach((path) => {
-        const arrowFunction = path.node.arguments[0];
-        if (
-          arrowFunction &&
-          arrowFunction.type === "ArrowFunctionExpression" &&
-          arrowFunction.body.type === "BlockStatement"
-        ) {
-          const body = arrowFunction.body.body;
-          arrowFunction.body.body = body.filter((node) => {
-            // rob:
-            // this matches straight await expression to gen('some task', { page, test }) inside a call expression
-
-            const resolveChainedMemberCall = (
-              e: MemberExpression
-            ): CallExpression | undefined => {
-              if (
-                e.object.type === "CallExpression" &&
-                e.object.callee.type === "MemberExpression"
-              ) {
-                return resolveChainedMemberCall(e.object.callee);
-              }
-
-              if (e.object.type === "CallExpression") {
-                return e.object;
-              }
-
-              if (
-                e.object.type === "AwaitExpression" &&
-                e.object.argument?.type === "CallExpression"
-              ) {
-                return e.object.argument;
-              }
-
-              return undefined;
-            };
-
-            const maybeChainedV =
-              node.type === "ExpressionStatement" &&
-              node.expression.type === "CallExpression" &&
-              node.expression.callee.type === "MemberExpression"
-                ? resolveChainedMemberCall(node.expression.callee)
-                : undefined;
-
-            // FIXME: this needs to be handled properly, maybe find a way to structurally reform this outside of this step.
-            //        e.g. extract the call to gen into a straight await expression and discard the call to the caller here.
-            //        atm. this is just a messy thing to cover expect(await gen(...)), expect(await gen(...))...toBe() or any other like
-            //        await someFunc(await gen(...))
-            if (
-              (node.type === "ExpressionStatement" &&
-                node.expression.type === "CallExpression" &&
-                node.expression.arguments?.length > 0 &&
-                node.expression.arguments[0].type === "AwaitExpression" &&
-                isGenCall(node.expression.arguments[0])) ||
-              (node.type === "ExpressionStatement" &&
-                node.expression.type === "AwaitExpression" &&
-                node.expression.argument?.type === "CallExpression" &&
-                node.expression.argument.arguments[0]?.type ===
-                  "AwaitExpression" &&
-                isGenCall(node.expression.argument.arguments[0])) ||
-              (maybeChainedV &&
-                maybeChainedV.arguments.length > 0 &&
-                maybeChainedV.arguments[0].type === "AwaitExpression" &&
-                maybeChainedV.arguments[0].argument?.type ===
-                  "CallExpression" &&
-                isGenCall(maybeChainedV.arguments[0]))
-            ) {
-              throw new Error(
-                "Gen2ESanitize gen2e code error, cannot sanitize gen2e calls as arguement to other call expressions"
-              );
-            }
-
-            // rob:
-            // this matches calls to gen that are done in the right hand of a variable declation.
-            // Matches let, const and var
-            // e.g. const value = gen('some task', { page, test })
-            if (
-              node.type === "VariableDeclaration" &&
-              node.declarations[0].type === "VariableDeclarator" &&
-              node.declarations[0].init?.type === "AwaitExpression" &&
-              isGenCall(node.declarations[0].init)
-            ) {
-              return true;
-            }
-
-            // rob:
-            // this matches straight await expression to gen('some task', { page, test })
-            if (
-              node.type === "ExpressionStatement" &&
-              node.expression.type === "AwaitExpression" &&
-              isGenCall(node.expression)
-            ) {
-              return true;
-            }
-
-            return false;
-          });
-        }
-      });
-
+    sanitizeGenCalls(root, j);
     return root.toSource();
   })(source);
--- packages/gen2e-interpreter/src/ast/pw-compile.ts
+++ packages/gen2e-interpreter/src/ast/pw-compile.ts
@@ -4,164 +4,179 @@ import { Gen2ECompileFunction, makeCompiler } from "./compiler";
 import { Gen2ELogger } from "@rhighs/gen2e-logger";
 import { Gen2ECompileError } from "../errors";
 
-const compiler = (
+const transformGenCall = (
+  j: any,
+  root: any,
   store: StaticStore,
   logger?: Gen2ELogger
-): Gen2ECompileFunction => {
-  const transform = (fileInfo: FileInfo, api: API): string => {
-    const j = api.jscodeshift;
-    const root = j(fileInfo.source);
+) => {
+  let titleWasSet = false;
 
-    let titleWasSet = false;
+  root
+    .find(j.CallExpression, {
+      callee: { name: "test" },
+      arguments: [{ type: "Literal" }],
+    })
+    .forEach((testPath: any) => {
+      const firstArg = testPath.node.arguments[0];
+      const testTitle =
+        firstArg.type === "Literal" ? firstArg.value : undefined;
 
-    // rob: replace gen("<task>", { page, test }) witgh the static playwright
-    //      expressions
-    root
-      .find(j.CallExpression, {
-        callee: { name: "test" },
-        arguments: [{ type: "Literal" }],
-      })
-      .forEach((testPath) => {
-        const firstArg = testPath.node.arguments[0];
-        const testTitle =
-          firstArg.type === "Literal" ? firstArg.value : undefined;
-        if (testTitle) {
-          j(testPath)
-            .find(j.AwaitExpression, {
-              argument: {
-                callee: { type: "Identifier", name: "gen" },
-                arguments: (args) =>
-                  args.length === 2 &&
-                  (args[0].type === "Literal" ||
-                    args[0].type === "StringLiteral" ||
-                    args[0].type === "TemplateLiteral") &&
-                  args[1].type === "ObjectExpression",
-              },
-            })
-            .replaceWith((path) => {
-              if (path.node.argument) {
-                const genFirstArg =
-                  path.node.argument.type === "CallExpression"
-                    ? path.node.argument.arguments[0]
-                    : undefined;
-                const genArg =
-                  genFirstArg?.type === "TemplateLiteral"
-                    ? genFirstArg.quasis[0].value.raw
-                    : genFirstArg?.type === "Literal" ||
-                      genFirstArg?.type === "StringLiteral"
-                    ? genFirstArg.value
-                    : undefined;
+      if (testTitle) {
+        j(testPath)
+          .find(j.AwaitExpression, {
+            argument: {
+              callee: { type: "Identifier", name: "gen" },
+              arguments: (args: any) =>
+                args.length === 2 &&
+                (args[0].type === "Literal" ||
+                  args[0].type === "StringLiteral" ||
+                  args[0].type === "TemplateLiteral") &&
+                args[1].type === "ObjectExpression",
+            },
+          })
+          .replaceWith((path: any) => {
+            if (path.node.argument) {
+              const genFirstArg =
+                path.node.argument.type === "CallExpression"
+                  ? path.node.argument.arguments[0]
+                  : undefined;
+              const genArg =
+                genFirstArg?.type === "TemplateLiteral"
+                  ? genFirstArg.quasis[0].value.raw
+                  : genFirstArg?.type === "Literal" ||
+                    genFirstArg?.type === "StringLiteral"
+                  ? genFirstArg.value
+                  : undefined;
 
-                if (genArg) {
-                  const ident = store.makeIdent(
-                    testTitle as string,
-                    genArg as string
-                  );
-                  const code = store.fetchStatic(ident);
+              if (genArg) {
+                const ident = store.makeIdent(
+                  testTitle as string,
+                  genArg as string
+                );
+                const code = store.fetchStatic(ident);
 
-                  if (code) {
-                    if (!code.expression) {
-                      const message = `got undefined or empty expression for ident`;
-                      if (logger) {
-                        logger.error(message, { ident });
-                      }
-                      throw new Gen2ECompileError(`${message} ${ident}`);
+                if (code) {
+                  if (!code.expression) {
+                    const message = `got undefined or empty expression for ident`;
+                    if (logger) {
+                      logger.error(message, { ident });
                     }
-
-                    if (
-                      testPath.node.arguments[0].type === "Literal" &&
-                      !titleWasSet
-                    ) {
-                      testPath.node.arguments[0].value = `gen2e:compiled-output - ${
-                        testTitle as string
-                      }`;
-                      titleWasSet = true;
-                    }
-
-                    const parsedInput = j(code.expression);
-                    const arrowFunction = parsedInput
-                      .find(j.ArrowFunctionExpression)
-                      .get().node;
-                    const body = arrowFunction.body;
-
-                    const internalArrowFunction = j.arrowFunctionExpression(
-                      [],
-                      body,
-                      true
-                    );
-                    internalArrowFunction.async = true;
-
-                    return j.awaitExpression(
-                      j.callExpression(
-                        j.parenthesizedExpression(internalArrowFunction),
-                        []
-                      )
-                    );
+                    throw new Gen2ECompileError(`${message} ${ident}`);
                   }
+
+                  if (
+                    testPath.node.arguments[0].type === "Literal" &&
+                    !titleWasSet
+                  ) {
+                    testPath.node.arguments[0].value = `gen2e:compiled-output - ${
+                      testTitle as string
+                    }`;
+                    titleWasSet = true;
+                  }
+
+                  const parsedInput = j(code.expression);
+                  const arrowFunction = parsedInput
+                    .find(j.ArrowFunctionExpression)
+                    .get().node;
+                  const body = arrowFunction.body;
+
+                  const internalArrowFunction = j.arrowFunctionExpression(
+                    [],
+                    body,
+                    true
+                  );
+                  internalArrowFunction.async = true;
+
+                  return j.awaitExpression(
+                    j.callExpression(
+                      j.parenthesizedExpression(internalArrowFunction),
+                      []
+                    )
+                  );
                 }
               }
+            }
 
-              return null;
-            });
-        }
-      });
+            return null;
+          });
+      }
+    });
+};
 
-    // rob: replace gen.test(({ page, gen }) => { ... }) with a native playwright
-    //      test expression, removing all dependencies from the gen IL library
-    root
-      .find(j.CallExpression, {
-        callee: {
-          type: "MemberExpression",
-          object: { name: "gen" },
-          property: { name: "test" },
+const transformGenTest = (j: any, root: any) => {
+  root
+    .find(j.CallExpression, {
+      callee: {
+        type: "MemberExpression",
+        object: { name: "gen" },
+        property: { name: "test" },
+      },
+      arguments: [
+        {
+          type: "ArrowFunctionExpression",
+          async: true,
+          params: [{ type: "ObjectPattern" }],
         },
-        arguments: [
-          {
-            type: "ArrowFunctionExpression",
-            async: true,
-            params: [{ type: "ObjectPattern" }],
-          },
-        ],
-      })
-      .replaceWith((path) => {
-        const arrowFunction = path.node.arguments[0];
-        if (arrowFunction && arrowFunction.type === "ArrowFunctionExpression") {
-          const params = arrowFunction.params;
-          if (
-            params.length === 1 &&
-            params[0].type === "ObjectPattern" &&
-            params[0].properties.some(
-              (prop) =>
+      ],
+    })
+    .replaceWith((path: any) => {
+      const arrowFunction = path.node.arguments[0];
+      if (arrowFunction && arrowFunction.type === "ArrowFunctionExpression") {
+        const params = arrowFunction.params;
+        if (
+          params.length === 1 &&
+          params[0].type === "ObjectPattern" &&
+          params[0].properties.some(
+            (prop: any) =>
+              prop.type === "Property" &&
+              prop.key.type === "Identifier" &&
+              prop.key.name === "page"
+          )
+        ) {
+          const newParams = j.objectPattern(
+            params[0].properties.filter(
+              (prop: any) =>
                 prop.type === "Property" &&
                 prop.key.type === "Identifier" &&
                 prop.key.name === "page"
             )
-          ) {
-            const newParams = j.objectPattern(
-              params[0].properties.filter(
-                (prop) =>
-                  prop.type === "Property" &&
-                  prop.key.type === "Identifier" &&
-                  prop.key.name === "page"
-              )
-            );
-            const result = j.arrowFunctionExpression(
-              [newParams],
-              arrowFunction.body,
-              true
-            );
-            result.async = true;
-            return result;
-          }
+          );
+          const result = j.arrowFunctionExpression(
+            [newParams],
+            arrowFunction.body,
+            true
+          );
+          result.async = true;
+          return result;
         }
-        return path.node;
-      });
-
-    return root.toSource();
-  };
-
-  return makeCompiler(transform);
+      }
+      return path.node;
+    });
 };
+
+const transform = (
+  fileInfo: FileInfo,
+  api: API,
+  store: StaticStore,
+  logger?: Gen2ELogger
+): string => {
+  const j = api.jscodeshift;
+  const root = j(fileInfo.source);
+
+  transformGenCall(j, root, store, logger);
+  transformGenTest(j, root);
+
+  return root.toSource();
+};
+
+const compiler = (
+  store: StaticStore,
+  logger?: Gen2ELogger
+): Gen2ECompileFunction =>
+  makeCompiler((fileInfo: FileInfo, api: API) =>
+    transform(fileInfo, api, store, logger)
+  );
 
 /**
  * Translates source code from the gen2e library into native playwright JavaScript code.
